43f445e56f9706d47aeca81d79e32857375cc943
Berit Carvalho <beritcarvalho@gmail.com>
2024-07-16 23:06:17 -0300

    - Adicionado Angular Material 18.1.0

--- CarteiraBalanceada.Web/src/app/app.module.ts
+++ CarteiraBalanceada.Web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeaturesModule } from './features/features.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { FeaturesModule } from './features/features.module';
     AppRoutingModule,
     FeaturesModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
