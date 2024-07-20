0b814fdf58a1adbdcfc1226170da820b18f20ccd
Berit Carvalho <beritcarvalho@gmail.com>
2024-07-19 23:27:59 -0300

    - Customizado botões da barra de ferramenta

--- CarteiraBalanceada.Web/src/app/shared/components/toobar/toobar.component.ts
+++ CarteiraBalanceada.Web/src/app/shared/components/toobar/toobar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-toobar',
@@ -7,5 +9,13 @@ import { FormControl } from '@angular/forms';
   styleUrl: './toobar.component.css'
 })
 export class ToobarComponent {
-  disableSelect = new FormControl(false);
+  botaoSelecionado = 0;
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('wallet', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/wallet.svg'));
+  }
+
+  ativarBotao(botao: number) {
+    this.botaoSelecionado = botao;
+  }
 }
