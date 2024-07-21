84a3ce75c4bf4999755ffc4408c7d04b95e9c510
Berit Carvalho <beritcarvalho@gmail.com>
2024-07-21 14:23:40 -0300

    Adicionado rotas as botões de navageção

--- CarteiraBalanceada.Web/src/app/shared/components/toobar/toobar.component.ts
+++ CarteiraBalanceada.Web/src/app/shared/components/toobar/toobar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toobar',
@@ -11,11 +12,19 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class ToobarComponent {
   botaoSelecionado = 0;
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer,
+    private router: Router) {
     iconRegistry.addSvgIcon('wallet', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/wallet.svg'));
   }
 
-  ativarBotao(botao: number) {
+  public ativarBotao(botao: number): void {
     this.botaoSelecionado = botao;
+    if (this.botaoSelecionado == 1) {
+      this.router.navigateByUrl('/resumo-carteira');
+    }
+    else if (this.botaoSelecionado == 2) {
+      this.router.navigateByUrl('/calculo-aporte');
+    }
   }
 }
