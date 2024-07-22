7bd45b42e33d9ecf64b173f1723fb8370bbd5c3e
Berit Carvalho <beritcarvalho@gmail.com>
2024-07-22 00:30:47 -0300

    Adicionado environments

---
+++ CarteiraBalanceada.Web/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  title: 'Local Environment',
+  carteiraBalanceadaApi: {
+    url: 'https://localhost:7189',
+    route: {
+      carteiras: '/carteiras'
+    }
+  }
+};
