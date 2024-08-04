5a3f184c2d5bdd2486b9fb280845cd569dad0d67
Berit Carvalho <beritcarvalho@gmail.com>
2024-08-04 17:29:52 -0300

    Adicionado funcionalidade para aportar

--- CarteiraBalanceada.Web/src/app/features/pages/resumo-carteira/components/realizar-aporte/realizar-aporte.component.ts
+++ CarteiraBalanceada.Web/src/app/features/pages/resumo-carteira/components/realizar-aporte/realizar-aporte.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, Input, input, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ValidadorEstadoFormulario } from '../../../../../shared/validators/validador-estado-formulario';
 import { Investimento } from '../../../../../shared/interfaces/investimento';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { UtilitariosService } from '../../../../../shared/services/utilitarios.service';
 import { catchError, empty, finalize } from 'rxjs';
 import { Carteira } from '../../../../../shared/interfaces/carteira';
@@ -14,14 +14,15 @@ import { CarteiraService } from '../../../../../shared/services/carteira.service
   styleUrl: './realizar-aporte.component.scss'
 })
 
-export class RealizarAporteComponent implements OnInit{
+export class RealizarAporteComponent implements OnInit {
   idCarteira!: string;
   investimento!: Investimento;
   aporteFormControl = new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+(,[0-9]+)?$/)]);
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-  private utilitariosService: UtilitariosService,
-  private carteiraSevice: CarteiraService
+  constructor(private dialogRef: MatDialogRef<RealizarAporteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private utilitariosService: UtilitariosService,
+    private carteiraSevice: CarteiraService
   ) {
     this.investimento = data.investimento;
   }
@@ -32,33 +33,33 @@ export class RealizarAporteComponent implements OnInit{
   matcher = new ValidadorEstadoFormulario();
 
   public aportar(): void {
-    const aporte: number = 1000;
-    const id: string = this.investimento?.id;
-    if (this.investimento?.id) {      
-      this.utilitariosService.setLoading(true);
-      this.carteiraSevice.aportarInvestimento(id, aporte).pipe(
-        catchError(error => {          
-          this.utilitariosService.mostrarNotificacaoErro(error?.message)
-          return empty();
-        }),
-        finalize(() => {
-          this.utilitariosService.setLoading(false);          
-        })
-      ).subscribe(() => {
-        this.utilitariosService.mostrarNotificacaoSucesso("Aporte realizado com Sucesso!");
-        if (this.idCarteira) {
-          this.carteiraSevice.distribuirAporte(this.idCarteira, 0);          
-        } 
-      });
-    }
-    else {
-      this.utilitariosService.notificarAlteracaoIdCarteira(null);
+    if (this.aporteFormControl.valid && this.aporteFormControl?.value) {
+      const aporte: number = Number(this.aporteFormControl.value.replace(',', '.'));
+      const id: string = this.investimento?.id;
+      if (this.investimento?.id) {
+        this.utilitariosService.setLoading(true);
+        this.carteiraSevice.aportarInvestimento(id, aporte).pipe(
+          catchError(error => {
+            this.utilitariosService.mostrarNotificacaoErro(error?.message)
+            return empty();
+          }),
+          finalize(() => {
+            this.utilitariosService.setLoading(false);
+            this.dialogRef.close();
+          })
+        ).subscribe(() => {
+          this.utilitariosService.mostrarNotificacaoSucesso("Aporte realizado com Sucesso!");
+          if (this.idCarteira) {
+            this.carteiraSevice.distribuirAporte(this.idCarteira, 0);
+          }
+        });
+      }
     }
   }
 
-  private observarCarteiraSelecionada(): void{
+  private observarCarteiraSelecionada(): void {
     this.utilitariosService.observarCarteiraSelecionada().subscribe(carteiraSelecionada => {
-      if(carteiraSelecionada) {
+      if (carteiraSelecionada) {
         this.idCarteira = carteiraSelecionada.id
       }
     });
